7259ed107826e1eaaa82980545baad764414dfb1
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-07-11 21:52:18 -0500

    feat: Added features section

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,8 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export function animateWithGsapTimeline(
   timeline,
   rotationRef,
@@ -14,4 +19,16 @@ export function animateWithGsapTimeline(
 
   timeline.to(firstTarget, { ...animationProps, ease: "power2.inOut" }, "<");
   timeline.to(secondTarget, { ...animationProps, ease: "power2.inOut" }, "<");
+}
+
+export function animateWithGsap(target, animationProps, scrollProps) {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverese",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
 }
